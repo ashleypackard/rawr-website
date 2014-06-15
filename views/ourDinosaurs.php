4a8578dbae4b7d50f6f215e81908511ecdf2ffb4
James Astbury <astburyj@wit.edu>
2014-06-15 17:02:15 -0400

    More additions and changes to Our Dinosaur

--- views/ourDinosaurs.php
+++ views/ourDinosaurs.php
@@ -4,55 +4,34 @@ echo "Here at R.A.W.R. we only offer herbivores. We have found that carnivores a
  are not good for families, and require a lot of feeding with expensive meat. However, carnivores are available
  upon special request.";
 
-print  '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-		<html>
-		<head>
-			<style>
-				.picture {
-					height:100px;
-					width:100px;
-					align:center;
-					margin:10px;
-				}
-				.rows {
-					margin:1px;
-					display:inline;
-					float:left;
-				}
-			</style>
-			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		</head>
-
-		<body>
-		
-		<div align="center">
+print  '<div align="center">
 			<div class="rows">
 				<img src="../assets/images/Barosaurus.jpg" alt="Barosaurus" class="picture"/></br>
-				<input id="btnBarosaurus" type="button" value="Barosaurus" align="center"></input>
+				<a id="btnBarosaurus" type="button" value="Barosaurus" align="center">Barosaurus</a>
 			</div>
 			<div class="rows">
 				<img src="../assets/images/diceratops.jpg" alt="Biceratops" class="picture"/></br>
-				<input id="btnDiceratops" type="button" value="Biceratops" align="center"></input>
+				<a id="btnDiceratops" type="button" value="Biceratops" align="center">Biceratops</a>
 			</div>
 			<div class="rows">
 				<img src="../assets/images/erketu.jpg" alt="Erketu" class="picture"/></br>
-				<input id="btnErketu" type="button" value="Erketu" align="center"></input>
+				<a id="btnErketu" type="button" value="Erketu" align="center">Erketu</a>
 			</div>
 			<div class="rows">
 				<img src="../assets/images/giraffatitan.jpg" alt="Giraffatitan" class="picture"/></br>
-				<input id="btnGiraffatitan" type="button" value="Giraffatitan" align="center"></input>
+				<a id="btnGiraffatitan" type="button" value="Giraffatitan" align="center">Giraffatitan</a>
 			</div>
 			<div class="rows">
 				<img src="../assets/images/laosaurus.jpg" alt="Laosaurus" class="picture"/></br>
-				<input id="btnLaosaurus" type="button" value="Laosaurus" align="center"></input>
+				<a id="btnLaosaurus" type="button" value="Laosaurus" align="center">Laosaurus</a>
 			</div>
 			<div class="rows">
 				<img src="../assets/images/manidens.jpg" alt="Manidens" class="picture"/></br>
-				<input id="btnManidens" type="button" value="Manidens" align="center"></input>
+				<a id="btnManidens" type="button" value="Manidens" align="center">Manidens</a>
+			</div>
+			<div class="rows">
+				<img src="../assets/images/minmi.jpg" alt="Minmi" class="picture"/></br>
+				<a id="btnMinmi" type="button" value="Minmi" align="center">Minmi</a>
 			</div>
 		</div>';
-
-print '</body>
-</html>';
-
 ?>
