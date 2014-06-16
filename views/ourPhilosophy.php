ea9b556fc2d6e13d5ed0badc9173f1b788204a48
Janelle Flaherty <flahertyj6@wit.edu>
2014-06-15 22:30:30 -0400

    basic static page

--- views/ourPhilosophy.php
+++ views/ourPhilosophy.php
@@ -1,5 +1,35 @@
 <?php
+print '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title> ourPhilosophy </title>
+<link rel="stylesheet" href=""../assets/stylesheets/layoutstyle.css"">
+<script language = "Javascript" type="text/Javascript" src="../assets/scripts/home_script"></script>
+</head> 
+<body>';
+print '<form action=”lab 5 lab05.php” method="post"> 
+<div id = "happy">
+<div id = "header1">The philosohpy</div>
+<div id = "header2">Happy Eggs and Happy People</div>
+<div id = "philosophy">
+<br></br>     
+<div id ="center"><img src="../assets/images/baby.jpg" alt="baby" height= 80% width= 80%></div>
+<p>&nbsp &nbsp Here at R.A.W.R, we are truly committed to the happiness of both our customers, and, our dinosaurs.
+Please join us for a tour Friday through Thursday 10AM to 5PM, and we know that not only will you be delighted by the things we have
+in store for you, you will realize you want one of our adorable eggs as well!</p>
+<br></br>
+<b>Our Hatcheries</b>
+<br></br>
+<p>
+Our hatcheries hatch dinosaurs with love and care. Only the best equipment is used to bring fourth their tiny delicate lives.
+Want to hatch them yourself? Have no fear, we can provide your very own hatching kit so you can watch your dinosaur break through
+its shell with your very own eyes. </p>
+<a href="www.someurl.com" class="" id="link">Read more about hatching your own dinosaur here!</a>
+</div>
+</div>
 
-echo "Philosophy ";
 
+ </form>'; 
+print '</body>
+</html>';
 ?>
