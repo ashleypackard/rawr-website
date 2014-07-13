dedb5a96e5c704b463a202c535b8e23d28e36316
Ashley Packard <ashleypackard27@gmail.com>
2014-07-13 02:12:53 -0400

    added egg kit section and js

--- views/credit.php
+++ views/credit.php
@@ -12,6 +12,12 @@
 			<li>http://www.jplegacy.org/jpencyclopedia/?p=6791</li>
 			<li>http://sweetclipart.com/tyrannosaurus-rex-dinosaur-1088</li>
 			<li>http://blog.smu.edu/research/2011/02/17/gender-gap-selection-bias-snubs-scholarly-achievements-of-female-scientists-with-fewer-awards-for-research/</li>
+			<li>http://incubatorwarehouse.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/g/e/genesisanglewht_2.jpg
+			</li>
+			<li>http://i1.squidoocdn.com/resize/squidoo_images/400/draft_lens19027496module156118496photo_1325483376pet_adoption_certificate</li>
+			<li>http://www.gotyourhandsfull.com/wp-content/uploads/2014/05/dinosaur-coloring-pages-online.gif</li>
+			<li>http://www.chicagoreader.com/imager/b/magnum/4331683/ab70/Key-Ingredient-007_magnum.jpg</li>
+			<li>http://media-cache-ak0.pinimg.com/236x/01/55/69/01556910cfb86aa4ee2da7ab08183e9d.jpg</li>
 
 		</ul>
 
--- views/purchase.php
+++ views/purchase.php
@@ -1,6 +1,6 @@
 <div id="store_div" class="row">
 	<div class="col-md-6">
-		 <h1>The Dino Store</h1>
+		 <h1 class="storeLabel" id="dinoStore">The Dino Store</h1>
 	</div>
 	<div class="col-md-6">
 		<p>Welcome to our Dino Store where you can purchase your own dinosaur to bring home! Purchase one of our many dinosaure egg kits, adopt a dinosaur, buy accessories, and beyond. This comprehensive store has everything you need for a happy dinosaur online, delivered right to your door.</p>
@@ -8,7 +8,7 @@
 </div>
 
 <div class="row">
-	<div class="col-md-12">
+	<div class="col-md-8">
 		<!-- Nav tabs -->
 		<ul class="nav nav-tabs" role="tablist">
 		  <li class="active"><a href="#adoption" role="tab" data-toggle="tab">Adoption</a></li>
@@ -24,68 +24,123 @@
 		  <div class="tab-pane active" id="adoption">
 		  	<div class="col-md-12">
 		  		<h1>Adoption - James</h1>
-		  		<p>Insert your store items here, use a table or just use the grid system from bootstrap, whatever you'd like!</p>
-
-		  		<p>When starting delete this two paragraphs and this is your whole area to work! If using the grid system make sure that you have &lt;div class="row"&gt; and then a &lt;div class="col-md-X"&gt; Note that all col-md 's must add up to 12 in each row. You can have any combination: 11 and 1, or 4, 2, 1, 2, 3. As long as it equals 12. You can even have a col-md-12. The md is just for medium, don't worry about it :D </p>
-
-		  		<p>Look at the code in the Our Dinosaurs page for help. Ask me or visit <a href="http://getbootstrap.com/css/#grid" target="_blank">Twitter Bootsrap Grid System</a> and <a href="http://getbootstrap.com/javascript/" target="_blank">Twitter Bootsrap JS</a> for cool ideas. I was thinking that we should use the  <a href="http://getbootstrap.com/javascript/#alerts" target="_blank">Alerts</a> for our store. Let me know if you have any questions!</p>
+		  		<p>Blah</p>
 		  	</div>
 		  </div>
 
 		  <div class="tab-pane" id="eggKits">
 		  	<div class="col-md-12">
-		  		<h1>Egg Kits - Ashley</h1>
-		  		<p>Insert your store items here, use a table or just use the grid system from bootstrap, whatever you'd like!</p>
+		  		<h3>Egg Kits</h3>
+		  		<div>
+		  			<p>Each of our egg kits comes with an egg, incubator, adoption certificate, and our starter food package.</p>
+		  			<table id="eggKitsTable">
+		  				<tbody>
+		  					<tr class="eggKitLabels">
+		  						<td>Barosaurus Egg Kit</td>
+		  						<td>Biceratops Egg Kit</td>
+		  						<td>Erketu Egg Kit</td>
+		  						<td>Giraffatitan Egg Kit</td>
+		  					</tr>
+		  					<tr>
+		  						<td><img src="../assets/images/Store-Eggkits/dino1-eggBasket.png" class="eggBasketPic"/></td>
+		  						<td><img src="../assets/images/Store-Eggkits/dino7-eggBasket.png" class="eggBasketPic"/></td>
+		  						<td><img src="../assets/images/Store-Eggkits/dino3-eggBasket.png" class="eggBasketPic"/></td>
+		  						<td><img src="../assets/images/Store-Eggkits/dino4-eggBasket.png" class="eggBasketPic"/></td>
+		  					</tr>
+		  					<tr>
+		  						<td>Price: $325.00</td>
+		  						<td>Price: $275.00</td>
+		  						<td>Price: $282.00</td>
+		  						<td>Price: $300.00</td>
+		  					</tr>
+		  					<tr>
+		  						<td><input type="number" min="0" value="0" max="30" class="addToBasket" name="Barosaurus-Egg-Kit" id="eggKits-325" /></td>
+		  						<td><input type="number" min="0" value="0" max="30" class="addToBasket" name="Biceratops-Egg-Kit" id="eggKits-275" /></td>
+		  						<td><input type="number" min="0" value="0" max="30" class="addToBasket" name="Erketu-Egg-Kit" id="eggKits-282" /></td>
+		  						<td><input type="number" min="0" value="0" max="30" class="addToBasket" name="Giraffatitan-Egg-Kit" id="eggKits-300" /></td>
+		  					</tr>
 
-		  		<p>When starting delete this two paragraphs and this is your whole area to work! If using the grid system make sure that you have &lt;div class="row"&gt; and then a &lt;div class="col-md-X"&gt; Note that all col-md 's must add up to 12 in each row. You can have any combination: 11 and 1, or 4, 2, 1, 2, 3. As long as it equals 12. You can even have a col-md-12. The md is just for medium, don't worry about it :D </p>
-		  		
-		  		<p>Look at the code in the Our Dinosaurs page for help. Ask me or visit <a href="http://getbootstrap.com/css/#grid" target="_blank">Twitter Bootsrap Grid System</a> and <a href="http://getbootstrap.com/javascript/" target="_blank">Twitter Bootsrap JS</a> for cool ideas. I was thinking that we should use the  <a href="http://getbootstrap.com/javascript/#alerts" target="_blank">Alerts</a> for our store. Let me know if you have any questions!</p>
+		  					<tr class="eggKitLabels" id="eggKitBottomRow">
+		  						<td>Laosaurus Egg Kit</td>
+		  						<td>Manidens Egg Kit</td>
+		  						<td>Minmi Egg Kit</td>
+		  					</tr>
+		  					<tr>
+		  						<td><img src="../assets/images/Store-Eggkits/dino5-eggBasket.png" class="eggBasketPic"/></td>
+		  						<td><img src="../assets/images/Store-Eggkits/dino2-eggBasket.png" class="eggBasketPic"/></td>
+		  						<td><img src="../assets/images/Store-Eggkits/dino6-eggBasket.png" class="eggBasketPic"/></td>
+		  					</tr>
+		  					<tr>
+		  						<td>Price: $410.00</td>
+		  						<td>Price: $299.00</td>
+		  						<td>Price: $389.00</td>
+		  					</tr>
+		  					<tr>
+		  						<td><input type="number" min="0" value="0" max="30" class="addToBasket" name="Laosaurus-Egg-Kit" id="eggKits-410" /></td>
+		  						<td><input type="number" min="0" value="0" max="30" class="addToBasket" name="Manidens-Egg-Kit" id="eggKits-299" /></td>
+		  						<td><input type="number" min="0" value="0" max="30" class="addToBasket" name="Minmi-Egg-Kit" id="eggKits-389" /></td>
+		  					</tr>
+		  				</tbody>
+		  			</table>
+		  		</div>
 		  	</div>
 		  </div>
 
 		  <div class="tab-pane" id="accessories">
 		  	<div class="col-md-12">
 		  		<h1>Accessories - Rob</h1>
-		  		<p>Insert your store items here, use a table or just use the grid system from bootstrap, whatever you'd like!</p>
-
-		  		<p>When starting delete this two paragraphs and this is your whole area to work! If using the grid system make sure that you have &lt;div class="row"&gt; and then a &lt;div class="col-md-X"&gt; Note that all col-md 's must add up to 12 in each row. You can have any combination: 11 and 1, or 4, 2, 1, 2, 3. As long as it equals 12. You can even have a col-md-12. The md is just for medium, don't worry about it :D </p>
-		  		
-		  		<p>Look at the code in the Our Dinosaurs page for help. Ask me or visit <a href="http://getbootstrap.com/css/#grid" target="_blank">Twitter Bootsrap Grid System</a> and <a href="http://getbootstrap.com/javascript/" target="_blank">Twitter Bootsrap JS</a> for cool ideas. I was thinking that we should use the  <a href="http://getbootstrap.com/javascript/#alerts" target="_blank">Alerts</a> for our store. Let me know if you have any questions!</p>
+		  		<p>Blah</p>
 		  	</div>
 		  </div>
 
 		  <div class="tab-pane" id="nutrition">
 		  	<div class="col-md-12">
 		  		<h1>Nutrition - Janelle</h1>
-		  		<p>Insert your store items here, use a table or just use the grid system from bootstrap, whatever you'd like!</p>
-
-		  		<p>When starting delete this two paragraphs and this is your whole area to work! If using the grid system make sure that you have &lt;div class="row"&gt; and then a &lt;div class="col-md-X"&gt; Note that all col-md 's must add up to 12 in each row. You can have any combination: 11 and 1, or 4, 2, 1, 2, 3. As long as it equals 12. You can even have a col-md-12. The md is just for medium, don't worry about it :D </p>
-		  		
-		  		<p>Look at the code in the Our Dinosaurs page for help. Ask me or visit <a href="http://getbootstrap.com/css/#grid" target="_blank">Twitter Bootsrap Grid System</a> and <a href="http://getbootstrap.com/javascript/" target="_blank">Twitter Bootsrap JS</a> for cool ideas. I was thinking that we should use the  <a href="http://getbootstrap.com/javascript/#alerts" target="_blank">Alerts</a> for our store. Let me know if you have any questions!</p>
+		  		<p>Blah</p>
 		  	</div>
 		  </div>
 		
 		  <div class="tab-pane" id="toys">
 		  	<div class="col-md-12">
 		  		<h1>Toys - Janelle</h1>
-		  		<p>Insert your store items here, use a table or just use the grid system from bootstrap, whatever you'd like!</p>
-
-		  		<p>When starting delete this two paragraphs and this is your whole area to work! If using the grid system make sure that you have &lt;div class="row"&gt; and then a &lt;div class="col-md-X"&gt; Note that all col-md 's must add up to 12 in each row. You can have any combination: 11 and 1, or 4, 2, 1, 2, 3. As long as it equals 12. You can even have a col-md-12. The md is just for medium, don't worry about it :D </p>
-		  		
-		  		<p>Look at the code in the Our Dinosaurs page for help. Ask me or visit <a href="http://getbootstrap.com/css/#grid" target="_blank">Twitter Bootsrap Grid System</a> and <a href="http://getbootstrap.com/javascript/" target="_blank">Twitter Bootsrap JS</a> for cool ideas. I was thinking that we should use the  <a href="http://getbootstrap.com/javascript/#alerts" target="_blank">Alerts</a> for our store. Let me know if you have any questions!</p>
+		  		<p>Blah</p>
 		  	</div>
 		  </div>
 
 		  <div class="tab-pane" id="bedding">
 		  	<div class="col-md-12">
 		  		<h1>Bedding - Ashley</h1>
-		  		<p>Insert your store items here, use a table or just use the grid system from bootstrap, whatever you'd like!</p>
-
-		  		<p>When starting delete this two paragraphs and this is your whole area to work! If using the grid system make sure that you have &lt;div class="row"&gt; and then a &lt;div class="col-md-X"&gt; Note that all col-md 's must add up to 12 in each row. You can have any combination: 11 and 1, or 4, 2, 1, 2, 3. As long as it equals 12. You can even have a col-md-12. The md is just for medium, don't worry about it :D </p>
-		  		
-		  		<p>Look at the code in the Our Dinosaurs page for help. Ask me or visit <a href="http://getbootstrap.com/css/#grid" target="_blank">Twitter Bootsrap Grid System</a> and <a href="http://getbootstrap.com/javascript/" target="_blank">Twitter Bootsrap JS</a> for cool ideas. I was thinking that we should use the  <a href="http://getbootstrap.com/javascript/#alerts" target="_blank">Alerts</a> for our store. Let me know if you have any questions!</p>
+		  		<p>Blah</p>
 		  	</div>
 		  </div>
 		</div>
 	</div>
+
+	<div class="col-md-4">
+		<div id="checkoutLane">
+				<h2 class="storeLabel">Your Basket</h2>
+			<div>
+				<p id="noItems">There are no items in your basket.<p>
+				<table id="storeBasket">
+					<thead>
+						<th class="basketHeaders">Item</th>
+						<th class="basketHeaders">Price</th>
+						<th class="basketHeaders">Quantity</th>
+						<th class="basketHeaders">Subtotal</th>
+					</thead>
+					<tbody>
+						<tr id="totalCol">
+							<td colspan="3">Total:</td>
+							<td id="totalPrice">$0.00</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 </div>
+
+
+<script>
+ $.getScript( "../assets/scripts/home_script.js" );
+</script>
