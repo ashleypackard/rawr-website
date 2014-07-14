68f95943c8241e60b8b4ae219941e88944f5f660
Janelle Flaherty <flahertyj6@wit.edu>
2014-07-13 20:51:31 -0400

    store for nutrition and toys

--- views/purchase.php
+++ views/purchase.php
@@ -56,23 +56,102 @@
 
 		  <div class="tab-pane" id="nutrition">
 		  	<div class="col-md-12">
-		  		<h1>Nutrition - Janelle</h1>
-		  		<p>Insert your store items here, use a table or just use the grid system from bootstrap, whatever you'd like!</p>
-
-		  		<p>When starting delete this two paragraphs and this is your whole area to work! If using the grid system make sure that you have &lt;div class="row"&gt; and then a &lt;div class="col-md-X"&gt; Note that all col-md 's must add up to 12 in each row. You can have any combination: 11 and 1, or 4, 2, 1, 2, 3. As long as it equals 12. You can even have a col-md-12. The md is just for medium, don't worry about it :D </p>
-		  		
-		  		<p>Look at the code in the Our Dinosaurs page for help. Ask me or visit <a href="http://getbootstrap.com/css/#grid" target="_blank">Twitter Bootsrap Grid System</a> and <a href="http://getbootstrap.com/javascript/" target="_blank">Twitter Bootsrap JS</a> for cool ideas. I was thinking that we should use the  <a href="http://getbootstrap.com/javascript/#alerts" target="_blank">Alerts</a> for our store. Let me know if you have any questions!</p>
+		  		<h1>Nutrition</h1>
+				<h3>To add an item to your basket, simple enter a quantity in the space below it. When you are done, hit "Add To Cart" at the bottom of the page.</h3>
+		  		 <div class="row">
+				 <h2>Dinosaur Treats</h2>
+				 <p>Treats should be given sparingly, but one should indulge their dino none the less!</p>
+				 <div class="col-md-X">
+				   <div class="col-md-2">Cow Bone $5.99<img src="../assets/images/dino-bone-all-natural-cow.jpg" alt="cow-bone" id = "picture"/><input type="text" size="8" name="cow-bone" id="5.99" class="addToBasket" value="" /></div>
+				   <div class="col-md-2">Gluten-Free $9.99 <img src="../assets/images/dino-bone-gluten-free.jpg" alt="gluten-free" id = "picture"/><input type="text" size="8" name="gluten-free" id="9.99" class="addToBasket" value="" /></div>
+                   <div class="col-md-2">Sheep Bone $9.99<img src="../assets/images/genuine-sheep.jpg" alt="sheep-bone" id = "picture"/><input type="text" size="8" name="sheep-bone" id="9.99" class="addToBasket" value="" /></div>
+                   <div class="col-md-2">Protein Bone $10.99<img src="../assets/images/protein-bone.jpg" alt="protein-bone" id = "picture"/><input type="text" size="8" name="protein-bone" id="10.99" class="addToBasket" value="" /></div>
+                   <div class="col-md-2">Fake Choc. Box $10.50<img src="../assets/images/choc-dino2.jpg" alt="fake-choc-dino" id = "picture"/><input type="text" size="8" name="fake-choc-dino" id="10.50" class="addToBasket" value="" /></div>
+                   <div class="col-md-2">Fake Choc. 5.99<img src="../assets/images/choc-dino.jpg" alt="fake-choc" id = "picture" /><input type="text" size="8" name="fake-choc" id="5.99" class="addToBasket" value="" /></div>
+				 </div>
+				 </div>
+				 <div class="row">
+				 <h2>Dinosaur Everyday Food</h2>
+				 <p>What everyday food your dino will require depends on your dino's breed. Some, need highly protein based food. For these, fresh meat or live food is highly recommended. Others need a steady diet of leaf-based vegetables. Any of the leafy vegetables here will suffice.</p>
+				 <div class="col-md-X">
+				   <div class="col-md-2">Single Rat $5.99<img src="../assets/images/live-rat.jpg" alt="rat" id = "picture"/><input type="text" size="8" name="rat" id="5.99" class="addToBasket" value="" /></div>
+				   <div class="col-md-2">Leaf Bag $3.49<img src="../assets/images/leaf-veggie-dinosaur.jpg" alt="dino-leaf" id = "picture"/><input type="text" size="8" name="dino-leaf" id="3.49" class="addToBasket" value="" /></div>
+				   <div class="col-md-2">Goat Steak $9.99<img src="../assets/images/goat-steak.jpg" alt="goat-steak" id = "picture"/><input type="text" size="8" name="goat-steak" id="9.99" class="addToBasket" value="" /></div>
+				   <div class="col-md-2">Dino Lettuce $2.99<img src="../assets/images/lettuce.jpg" alt="dino-lettuce" id = "picture"/><input type="text" size="8" name="dino-lettuce" id="2.99" class="addToBasket" value="" /></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				 </div>
+				 </div>
+				 <div class="row">
+				 <h2>Dinosaur Supplements</h2>
+				 <p>Supplements will keep your dino healthy for many years to come. Consider investing in some today!</p>
+				 <p>Please remember to follow all instructions carefully when using supplements</p>
+				 <div class="col-md-X">
+				   <div class="col-md-2">Supplement $20.99<img src="../assets/images/all-natural-supplement.jpeg" alt="supplement" id = "picture"/><input type="text" size="8" name="supplement" id="20.99" class="addToBasket" value="" /></div>
+				   <div class="col-md-2">Vitamin C $25.99<img src="../assets/images/dino-vita-c-for-colds.jpg" alt="vita-c" id = "picture"/><input type="text" size="8" name="vita-c" id="25.99" class="addToBasket" value="" /></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				 </div>
+				 </div>
+				 <div class="row">
+				 <h2>Other Nutrition</h2>
+				 <p>Check out our grow-your-own leaf tree for the everyday feeding of vegetarian dinosaurs, in stock for a limited time.</p>
+				 <div class="col-md-X">
+				   <div class="col-md-2">Grow Your Own Leaf Tree $30.99<img src="../assets/images/tree-grow-your-own.jpg" alt="tree" id = "picture"/><input type="text" size="8" name="tree" id="30.99" class="addToBasket" value="" /></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				 </div>
+				 </div>
+				 <button class = 'button' id="addtocart"> Add To Cart </button>
 		  	</div>
 		  </div>
 		
 		  <div class="tab-pane" id="toys">
 		  	<div class="col-md-12">
-		  		<h1>Toys - Janelle</h1>
-		  		<p>Insert your store items here, use a table or just use the grid system from bootstrap, whatever you'd like!</p>
-
-		  		<p>When starting delete this two paragraphs and this is your whole area to work! If using the grid system make sure that you have &lt;div class="row"&gt; and then a &lt;div class="col-md-X"&gt; Note that all col-md 's must add up to 12 in each row. You can have any combination: 11 and 1, or 4, 2, 1, 2, 3. As long as it equals 12. You can even have a col-md-12. The md is just for medium, don't worry about it :D </p>
-		  		
-		  		<p>Look at the code in the Our Dinosaurs page for help. Ask me or visit <a href="http://getbootstrap.com/css/#grid" target="_blank">Twitter Bootsrap Grid System</a> and <a href="http://getbootstrap.com/javascript/" target="_blank">Twitter Bootsrap JS</a> for cool ideas. I was thinking that we should use the  <a href="http://getbootstrap.com/javascript/#alerts" target="_blank">Alerts</a> for our store. Let me know if you have any questions!</p>
+		  		<h1>Toys</h1>
+				<h3>To add an item to your basket, simple enter a quantity in the space below it. When you are done, hit "Add To Cart", at the bottom of the page.</h3>
+		  		 <div class="row">
+				 <h2>Teeth health</h2>
+				 <p>Get a chew toy to prevent chewing damage to home furniture!</p>
+				 <div class="col-md-X">
+				   <div class="col-md-2">Chew Rex $9.99<img src="../assets/images/chew-toy-rex.jpg" alt="toy-rex" id = "picture"/><input type="text" size="8" name="toy-rex" id="9.99" class="addToBasket" value="" /></div>
+				   <div class="col-md-2">Vegisaurus#1 $10.99<img src="../assets/images/veg-toy.jpg" alt="veg-toy" id = "picture"/><input type="text" size="8" name="veg-toy1" id="10.99" class="addToBasket" value="" /></div>
+				   <div class="col-md-2">Vegisaurus#2 $10.99<img src="../assets/images/veg-toy-2.jpg" alt="veg-toy2" id = "picture"/><input type="text" size="8" name="veg-toy2" id="10.99" class="addToBasket" value="" /></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				 </div>
+				 </div>
+				 <div class="row">
+				 <h2>Stuffed Pals</h2>
+				 <p>Buy a stuffed friend to keep your dinosaur company (or throw when you feel like playing fetch)</p>
+				 <div class="col-md-X">
+				   <div class="col-md-2">Triceretops $15.99<img src="../assets/images/stuff-pal1.jpg" alt="tri-pal" id = "picture"/> <input type="text" size="8" name="triceretops-pal" id="15.99" class="addToBasket" value="" /></div>
+				   <div class="col-md-2">Long Neck#1 $15.99<img src="../assets/images/stuff-pal.jpg" alt="neck-pal" id = "picture"/> <input type="text" size="8" name="long-neck-pal" id="15.99" class="addToBasket" value=""/></div>
+				   <div class="col-md-2">Flier $15.99<img src="../assets/images/pal-3.jpg" alt="fly-pal" id = "picture"/><input type="text" size="8" name="flying-pal-pal" id="15.99" class="addToBasket" value=""/></div>
+				   <div class="col-md-2">Long Neck#2 $15.99<img src="../assets/images/pal5.jpg" alt="neck-pal1" id = "picture"/><input type="text" size="8" name="long-neck-pal-1" id="15.99" class="addToBasket" value=""/></div>
+				   <div class="col-md-2">Long Neck#3 $15.99<img src="../assets/images/pal6.jpg" alt="neck-pal2" id = "picture"/><input type="text" size="8" name="long-neck-pal-2" id="15.99" class="addToBasket" value=""/></div>
+				   <div class="col-md-2"></div>
+				 </div>
+				 </div>
+				 <div class="row">
+				 <h2>Other Toys</h2>
+				 <p>This section is for limited time items ranging from throwing balls to other toys.</p>
+				 <div class="col-md-X">
+				   <div class="col-md-2">Throwing Balls $5.99<img src="../assets/images/throwing-balls.jpg" alt="balls" id = "picture"/><input type="text" size="8" name="balls" id="5.99" class="addToBasket" value="" /></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				   <div class="col-md-2"></div>
+				 </div>
+				 </div>
+				 <button class = 'button' id="addtocart"> Add To Cart </button>
 		  	</div>
 		  </div>
 
