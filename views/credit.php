76efcb408e98679ca5636e9ecf2ea3d7ee0a2130
Janelle Flaherty <flahertyj6@wit.edu>
2014-07-13 20:59:44 -0400

    credit update

--- views/credit.php
+++ views/credit.php
@@ -12,7 +12,13 @@
 			<li>http://www.jplegacy.org/jpencyclopedia/?p=6791</li>
 			<li>http://sweetclipart.com/tyrannosaurus-rex-dinosaur-1088</li>
 			<li>http://blog.smu.edu/research/2011/02/17/gender-gap-selection-bias-snubs-scholarly-achievements-of-female-scientists-with-fewer-awards-for-research/</li>
-
+            <li>http://www.theuncommondog.com/godog-dino-bruto-with-chew-guard-dog-toy-green.aspx<li>
+			<li>http://www.petco.com/product/100793/American-Prime-Cuts-Mammoth-Dog-Bone.aspx</li>
+			<li>http://www.muttropolis.com/products2.cfm/ID/10013/name/Dinosaur-Dog-Toy-with-Chew-Guard</li>
+			<li>https://www.google.com/search?q=dinosaur+chew+toys&tbm=isch&source=lnms&sa=X&ei=tBbDU8HaO8GwyASQh4DABw&ved=0CAYQ_AUoAQ&biw=1538&bih=796#facrc=_&imgdii=asNCpr5yvoQpkM%3A%3B-fBNbTaR9Ld3uM%3BasNCpr5yvoQpkM%3A&imgrc=asNCpr5yvoQpkM%253A%3BlpWxbm6AkObYnM%3Bhttp%253A%252F%252Fecx.images-amazon.com%252Fimages%252FI%252F31dyuI311FL._SX300_.jpg%3Bhttp%253A%252F%252Fwww.amazon.com%252FCharming-Dino-Triceratops-Rubber-Dinosaur%252Fdp%252FB00CJ8OZ4S%3B300%3B200<li>
+			<li>https://www.google.com/search?q=dinosaur+chew+toys&tbm=isch&source=lnms&sa=X&ei=tBbDU8HaO8GwyASQh4DABw&ved=0CAYQ_AUoAQ&biw=1538&bih=796#facrc=_&imgdii=asNCpr5yvoQpkM%3A%3B95LtAifIu0OHGM%3BasNCpr5yvoQpkM%3A&imgrc=asNCpr5yvoQpkM%253A%3BlpWxbm6AkObYnM%3Bhttp%253A%252F%252Fecx.images-amazon.com%252Fimages%252FI%252F31dyuI311FL._SX300_.jpg%3Bhttp%253A%252F%252Fwww.amazon.com%252FCharming-Dino-Triceratops-Rubber-Dinosaur%252Fdp%252FB00CJ8OZ4S%3B300%3B200</li>
+		    <li>http://www.amazon.com/Nylabone-Durable-Dental-Dinosaur-Varies/dp/B000084E6V</li>
+		    <li>http://www.google.com/imgres?imgurl=&imgrefurl=https%3A%2F%2Fp2p.paperlesstrans.com%2Fdefault.aspx%3Fi%3Dsatstorefront&h=0&w=0&tbnid=MHuMGX03OQr5XM&zoom=1&tbnh=220&tbnw=220&docid=ZV-9rPqzKBQLVM&tbm=isch&ei=rgbDU-PbCcmdyASIv4HABg&ved=0CAIQsCUoAA</li>
 		</ul>
 
 	</div>
