e723be7a836869b07e3ebd4dbed1e033ad6cffc3
Ashley Packard <ashleypackard27@gmail.com>
2014-07-18 23:09:04 -0400

    add new branch and start working on db files

--- assets/scripts/getQuantities.php
+++ assets/scripts/getQuantities.php
@@ -1,39 +1,32 @@
 <?php
 
-$con=mysqli_connect("localhost","root","","donutshop");
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  print json_encode(array("success"=>false));
-  exit();
-}
+	$con=mysqli_connect("localhost","rawr-client","","rawr-db");
+	// Check connection
+	if (mysqli_connect_errno()) {
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	  print json_encode(array("success"=>false));
+	  exit();
+	}
 
-// Variables for donut_shop
-$donutshopID = (int)$_POST['donutShopID'];
-$street = mysqli_real_escape_string($con, $_POST['street']);
-$city = mysqli_real_escape_string($con, $_POST['city']);
-$state = mysqli_real_escape_string($con, $_POST['state']);
-$zip = (int)$_POST['zip'];
-$telephone = mysqli_real_escape_string($con, $_POST['telephone']);
-$ownRented = mysqli_real_escape_string($con, $_POST['ownedOrRented']);
-$bakery = mysqli_real_escape_string($con, $_POST['bakery']);
+	// Display messages
+	$result = mysqli_query($con,"SELECT item_name, stock_quantity FROM `bedding`");
 
-// variables for donut
-$donutID = (int)$_POST['donutID'];
-$desc = mysqli_real_escape_string($con, $_POST['description']);
-$cal = (int)$_POST['calories'];
-$fat = (int)$_POST['fatGrams'];
-$yearInvent = (int)$_POST['yearInvented'];
-$yearInInven = (int)$_POST['yearsInInventory'];
+	$num_rows = mysqli_num_rows($result);
 
-mysqli_query($con,"INSERT INTO donut_shop (DonutShopID, Street, City, State, Zip, Telephone, OwnedOrRented, Bakery)
-VALUES ('$donutshopID', '$street', '$city', '$state', '$zip', '$telephone', '$ownRented', '$bakery')");
+	if($num_rows == 0)
+		echo "<p id='rowCounter'>There are no messages posted.</p>";
+	else if($num_rows == 1)
+		echo "<p id='rowCounter'>There is " . $num_rows . " message posted.</p>";
+	else
+		echo "<p id='rowCounter'>There are " . $num_rows . " messages posted.</p>";
 
-mysqli_query($con,"INSERT INTO donut (DonutShopID, DonutID, Description, Calories, FatGrams, YearInvented, YearsInInventory) 
-VALUES ('$donutshopID', '$donutID', '$desc', '$cal', '$fat', '$yearInvent', '$yearInInven')");
+	while($row = mysqli_fetch_array($result)) {
+	  echo "<div class='post'> <h3 class='subjectHeader'>" . $row['subject'] . "</h3>";
+	  echo "<p>" . $row['body'] . "</p></div>";
+	}
 
-mysqli_close($con);
+	mysqli_close($con);
 
-print json_encode(array("success"=>true));
+	print json_encode(array("success"=>true));
 
 ?>
